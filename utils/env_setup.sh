4e9ae273219b90551a37a28f02ac9ac07d03b9f2
Sreejith Unnikrishnan <srunnikr@eng.ucsd.edu>
2015-10-31 22:26:58 -0700

    Scala build appended

--- utils/env_setup.sh
+++ utils/env_setup.sh
@@ -30,7 +30,27 @@ echo ""
 echo "############## Setting up Java on the machine #####################"
 sudo apt-get install openjdk-7-jre-headless
 sudo apt-get install openjdk-7-jdk
+echo "###################################################################"
+echo ""
+
+echo "################ Installing scala ################################"
 cd ~
+sudo apt-get install scala
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update
+sudo apt-get install sbt
+export SCALA_HOME="/usr/bin/scala"
+echo "###################################################################"
+echo ""
+
+echo "################ Building varys ################################"
+cd ~/varys/
+./sbt/sbt package
+echo "Varys built"
+cd ~
+echo "###################################################################"
+echo ""
 
 echo "############ Installing FNSS and python dependencies ##############"
 sudo apt-get install python-numpy python-scipy
