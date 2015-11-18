5efc3bc703ef27422f040296d523c6e7a1803564
Ruby Pai <rjpai@eng.ucsd.edu>
2015-11-18 11:37:14 -0800

    Added receiveHostName to SimEventDesc, so all needed info is being passed from controller. Added terminate method to controller. Moved test to its own method.

--- controller/src/main/java/Controller/SimEventDesc.java
+++ controller/src/main/java/Controller/SimEventDesc.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import javax.mail.search.SentDateTerm;
 import java.io.Serializable;
 
 /**
@@ -7,18 +8,32 @@ import java.io.Serializable;
  */
 public class SimEventDesc implements Serializable {
 
-    public SimEventType event;
+    public SimEventType event;  // SEND, RECEIVE, TERMINATE
+    // fields below only valid when event is SEND or RECEIVE
     public int numBytes;
+    public String receiveHostName;  // only needed when SimEventType is SEND
     public int portNumber;
 
-    public SimEventDesc(SimEventType event, int numBytes, int portNum) {
+    public SimEventDesc(SimEventType event, int numBytes, String receiveHostName, int portNum) {
         this.event = event;
         this.numBytes = numBytes;
+        this.receiveHostName = receiveHostName;
         this.portNumber = portNum;
     }
 
     public String toString() {
-        return (event.toString() + " " + numBytes + " bytes");
+        String desc = null;
+        switch (event) {
+            case SEND:
+            case RECEIVE:
+                desc = (event.toString() + " " + numBytes + " bytes, " +
+                        "receiver: " + receiveHostName + ":" + portNumber + " ");
+                break;
+            case TERMINATE:
+                desc = "TERMINATE";
+                break;
+        }
+        return desc;
     }
 
 }
