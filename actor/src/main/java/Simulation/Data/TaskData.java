432f74f3a63ed4657156c0138bf2ae4327c4265d
srunnikr <srunnikr@eng.ucsd.edu>
2015-11-19 11:43:31 -0800

    Added new slave class code and task class

--- actor/src/main/java/Simulation/Data/TaskData.java
+++ actor/src/main/java/Simulation/Data/TaskData.java
@@ -1,7 +1,30 @@
 package Simulation.Data;
 
+import com.sun.javafx.tk.Toolkit;
+
 /**
  * Created by stanislavmushits on 19/11/15.
  */
 public class TaskData {
+    String data;
+    long bytes;
+
+    public void generateData(long numBytes) {
+        // Creates a string with proper size
+        // One char is 1 Byte. Using StringBuffer to improve memory performance
+        StringBuffer outputBuffer = new StringBuffer();
+        for (int i = 0; i<this.bytes; ++i) {
+            outputBuffer.append('a');
+        }
+        this.data = outputBuffer.toString();
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public TaskData(long bytes) {
+        this.bytes = bytes;
+        generateData(bytes);
+    }
 }
--- actor/src/main/java/Simulation/Slave/Slave.java
+++ actor/src/main/java/Simulation/Slave/Slave.java
@@ -1,7 +1,63 @@
 package Simulation.Slave;
 
+import Simulation.Data.SimTask;
+import Simulation.Data.TaskData;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Created by stanislavmushits on 19/11/15.
  */
 public class Slave {
+
+    SimTask task;
+    InetAddress myIpAddress;
+    boolean isReceiver;
+
+    Slave(SimTask t) throws UnknownHostException {
+        this.task = t;
+        this.myIpAddress = InetAddress.getLocalHost();
+    }
+
+    public boolean AmIReceiver() {
+        return task.dstAddress.equals(myIpAddress.toString());
+    }
+
+    public void GenerateData() {
+        if (!AmIReceiver()) {
+            // Sender, so create data
+            task.data = new TaskData(task.size);
+        }
+    }
+
+    public void run() {
+
+        //parse task
+
+        // Check if receiver
+        this.isReceiver = AmIReceiver();
+
+        // if sender, call generate data
+        if (!this.isReceiver) {
+            GenerateData();
+        }
+
+        // send the task based on varys or traditional
+        if (this.task.simulationType == SimTask.SimulationType.VARYS) {
+            if (this.isReceiver) {
+                // Call receive task of varys
+            } else {
+                // Call send task of varys
+            }
+        } else {
+            if (this.isReceiver) {
+                // Call receive task of traditional
+            } else {
+                // Call send task of traditional
+            }
+        }
+
+    }
+
 }
