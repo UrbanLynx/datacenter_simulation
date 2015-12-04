d3b7ec72face97ac87c9b2d98279bfd4c62f5bc0
Stanislav <stasmushits@gmail.com>
2015-12-04 12:41:34 -0800

    Added (1) sending and receiving in parallel

--- actor/src/main/java/Simulation/Communicators/Communicator.java
+++ actor/src/main/java/Simulation/Communicators/Communicator.java
@@ -13,6 +13,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.CountDownLatch;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -75,24 +76,24 @@ public class Communicator {
         return "REDUCER_" + task.coflowId+ "_" + task.currentSlaveId;
     }
 
-    public void send(SimTask task) {
+    public void send(final SimTask task) {
         try {
             //Utils.logger.log(Level.INFO, String.format("%1$s, task %2$d, slave %3$d started with coflow %4$d",
             //        "MAPPER", task.id, task.currentSlaveId, InetAddress.getLocalHost().toString(), task.coflowId));
 
 
 
-            int coflowId = 0;
+            //int coflowId = 0;
             //VarysListener listener;
             //VarysClient client;
-            if (task.simulationType == SimulationType.VARYS){
+            //if (task.simulationType == SimulationType.VARYS){
                 //listener = new VarysListener();
                 //client = new VarysClient(getSenderId(task), task.masterUrl, listener);
                 //client.start();
                 //CoflowDescription desc = new CoflowDescription("DEFAULT"+task.id, CoflowType.DEFAULT(), -1, -1);
                 //int coflowId = client.registerCoflow(desc);
-                coflowId = Integer.parseInt(task.coflowId);
-            }
+            //    coflowId = Integer.parseInt(task.coflowId);
+            //}
 
 
             //Utils.safePrintln("Master client id " + client.masterClientId());
@@ -100,52 +101,74 @@ public class Communicator {
             //DataGenerator generator = new DataGenerator();
             //generator.generateUnitObject(1024);
 
-            for (Reducer reducer: task.reducersArr){
+            final CountDownLatch latch = new CountDownLatch(task.reducersArr.size());
+            for (final Reducer newReducer: task.reducersArr){
                 //RP removed
-                //Utils.logger.log(Level.INFO, getSendLogContent(task,reducer, "trying to connect"));
-                Socket socket = Utils.connectTo(reducer.address, reducer.port, 2000);
+                Utils.logger.log(SimLogger.LogLevel.INFO, getSendLogContent(task,newReducer, "trying to connect"));
 
-                InputStream inputStream = socket.getInputStream();
-                //RP removed
-                //Utils.logger.log(Level.INFO, getSendLogContent(task,reducer, "connected"));
+                Thread t = new Thread(){
+                    //public Socket socket;
+                    @Override
+                    public void run(){
+                        try{
+                            Reducer reducer = newReducer;
+                            Socket socket = Utils.connectTo(reducer.address, reducer.port, 2000);
 
-                OutputStream output = null;
-                if (task.simulationType == SimulationType.TRADITIONAL){
-                    output = socket.getOutputStream();
-                } else{
-                    if (doUseCoflow(reducer, task)){
-                        Utils.safePrintln(getSendLogContent(task,reducer, "use coflow"));
-                        output = new VarysOutputStream(socket, coflowId);
+                            InputStream inputStream = socket.getInputStream();
+                            //RP removed
+                            Utils.logger.log(SimLogger.LogLevel.INFO, getSendLogContent(task,reducer, "connected"));
 
-                    } else{
-                        Utils.safePrintln(getSendLogContent(task,reducer, "no coflow"));
-                        output = socket.getOutputStream();
+                            OutputStream output = null;
+                            if (task.simulationType == SimulationType.TRADITIONAL){
+                                output = socket.getOutputStream();
+                            } else{
+                                if (doUseCoflow(reducer, task)){
+                                    Utils.safePrintln(getSendLogContent(task,reducer, "use coflow"));
+                                    output = new VarysOutputStream(socket, Integer.parseInt(task.coflowId));
+
+                                } else{
+                                    Utils.safePrintln(getSendLogContent(task,reducer, "no coflow"));
+                                    output = socket.getOutputStream();
+                                }
+                            }
+
+
+                            //RP removed
+                            //Utils.logger.log(Level.INFO, getSendLogContent(task,reducer, "attempt to send"));
+
+                            //long timeStamp = System.currentTimeMillis();
+                            byte[] buf = new byte[1024];
+                            for (int i=0; i < reducer.sizeKB; i++){
+                                output.write(buf);
+                                output.flush();
+                            }
+
+
+                            //RP removed
+                            //Utils.logger.log(Level.INFO, String.valueOf(timeStamp)+getSendLogContent(task,reducer, "finished"));
+
+                            // log send time for results analysis
+                            long timeStamp = System.currentTimeMillis();
+                            Utils.logger.log(SimLogger.LogLevel.ANALYS, LogUtils.getSlaveLogContent(task, LogUtils.Event.SEND, Long.toString(timeStamp)));
+
+                            output.close();
+                            socket.close();
+
+                            Utils.safePrintln("[Mapper" + task.currentSlaveTaskIndex + "] finished with reducer " +
+                                    reducer.address + ":" + reducer.port);
+                            latch.countDown();
+                        }
+                        catch(Exception e){
+                            Utils.safePrintln(e.getMessage());
+                            e.printStackTrace();
+                        }
+
                     }
-                }
+                };
+                t.start();
 
-
-                //RP removed
-                //Utils.logger.log(Level.INFO, getSendLogContent(task,reducer, "attempt to send"));
-
-                //long timeStamp = System.currentTimeMillis();
-                byte[] buf = new byte[1024];
-                for (int i=0; i < reducer.sizeKB; i++){
-                    output.write(buf);
-                    output.flush();
-                }
-
-
-                //RP removed
-                //Utils.logger.log(Level.INFO, String.valueOf(timeStamp)+getSendLogContent(task,reducer, "finished"));
-
-                // log send time for results analysis
-                long timeStamp = System.currentTimeMillis();
-                Utils.logger.log(SimLogger.LogLevel.ANALYS, LogUtils.getSlaveLogContent(task, LogUtils.Event.SEND, Long.toString(timeStamp)));
-
-                output.close();
-                socket.close();
             }
-
+            latch.await();
         } catch (Exception e) {
             Utils.safePrintln(e.getMessage());
             e.printStackTrace();
@@ -156,46 +179,73 @@ public class Communicator {
         return !reducer.doNotRegisterFlow.contains(task.currentSlaveTaskIndex);
     }
 
-    public void receive(SimTask task) {
+    public class ReceiveThread implements Runnable {
+
+        Socket socket;
+        CountDownLatch latch;
+
+        public ReceiveThread(Socket socket, CountDownLatch latch) {
+            // store parameter for later user
+        }
+
+        public void run() {
+        }
+    }
+
+    public void receive(final SimTask task) {
         try {
             ServerSocket serverSocket = new ServerSocket(task.currentSlavePort);
+            final CountDownLatch latch = new CountDownLatch(task.mappers.size());
             //Utils.safePrintln("Accepting on port "+task.currentSlavePort);
 
             int receivedNumberTimes = 0;
             while (receivedNumberTimes != task.mappers.size()){
-                //RP removed
+
                 //Utils.logger.log(Level.INFO, getReceiveLogContent(task,task.reducers.get(task.currentSlaveId), "accepting"));
 
-                Socket socket = serverSocket.accept();
+                final Socket newSocket = serverSocket.accept();
+                Thread t = new Thread(){
+                    //public Socket socket;
+                    @Override
+                    public void run(){
+                        try {
+                            Socket socket = newSocket;
+                            OutputStream outputStream = socket.getOutputStream();
+                            outputStream.flush();
+                            //ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+                            InputStream inputStream = socket.getInputStream();
 
-                OutputStream outputStream = socket.getOutputStream();
-                outputStream.flush();
-                //ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-                InputStream inputStream = socket.getInputStream();
-                //RP removed
-                //Utils.logger.log(Level.INFO, getReceiveLogContent(task,task.reducers.get(task.currentSlaveId), "connected"));
+                            //Utils.logger.log(Level.INFO, getReceiveLogContent(task,task.reducers.get(task.currentSlaveId), "connected"));
 
-                //byte[] data = new byte[(int)task.reducers.get(task.currentSlaveId).sizeBytes()];
-                byte[] data = new byte[1024];
-                int n = 0;
-                for(int i=0; i< (int)task.reducers.get(task.currentSlaveId).sizeKB; i++){
-                    n += inputStream.read(data);
-                }
+                            //byte[] data = new byte[(int)task.reducers.get(task.currentSlaveId).sizeBytes()];
+                            byte[] data = new byte[1024];
+                            int n = 0;
+                            for(int i=0; i< (int)task.reducers.get(task.currentSlaveId).sizeKB; i++){
+                                n += inputStream.read(data);
+                            }
 
-                long timeStamp = System.currentTimeMillis();
-                Utils.logger.log(SimLogger.LogLevel.ANALYS, LogUtils.getSlaveLogContent(task, LogUtils.Event.RECEIVE, Long.toString(timeStamp) ));
+                            long timeStamp = System.currentTimeMillis();
+                            Utils.logger.log(SimLogger.LogLevel.ANALYS, LogUtils.getSlaveLogContent(task, LogUtils.Event.RECEIVE, Long.toString(timeStamp) ));
 
-                //RP removed
-                //Utils.logger.log(Level.INFO, String.valueOf(timeStamp)+getReceiveLogContent(task,task.reducers.get(task.currentSlaveId), "data received"));
 
-                inputStream.close();
-                socket.close();
+                            //Utils.logger.log(Level.INFO, String.valueOf(timeStamp)+getReceiveLogContent(task,task.reducers.get(task.currentSlaveId), "data received"));
 
-                Utils.safePrintln("[Reducer]: Got " + n + " bytes.");
+                            inputStream.close();
+                            socket.close();
 
+                            Utils.safePrintln("[Reducer" + task.currentSlaveTaskIndex + "]: Got " + n + " bytes.");
+                            latch.countDown();
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
                 receivedNumberTimes++;
             }
 
+            latch.await();
             serverSocket.close();
 
             Utils.reportFinishToMaster(task);
